6f3da5135bc8ecdacbe2f5b4dcddbfe323c4ea7f
Ian Denhardt <ian@zenhack.net>
2017-03-02 20:26:05 -0500

    Switch terminal to st

--- config.h
+++ config.h
@@ -55,7 +55,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static const char *dmenucmd[] = { "dmenu_run", "-fn", font, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "sakura", NULL };
+static const char *termcmd[]  = { "st", NULL };
 
 static const char *volup[]   = {"vol-adjust", "up", NULL };
 static const char *voldown[] = {"vol-adjust", "down", NULL };
